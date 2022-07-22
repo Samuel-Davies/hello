fda85deaf65d8f8403539a480f980c192164ab4b
Samuel-Davies <rockydavies21@gmail.com>
2022-07-22 16:29:11 +0000

    created  dynamic url

--- helloWorld/views.py
+++ helloWorld/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def index(request, name):
+    return render(request, 'index.html', {
+        'name':name
+    })
